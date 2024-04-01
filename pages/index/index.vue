f0ba6da525be1d3159dfb4540aab947e2d720109
MingYu-tju <1597232338@qq.com>
2024-04-01 16:53:03 +0800

    搜索结果

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,32 +1,37 @@
 <template>
-	<view class="content">
-		
+	<view class="page">
+
 		<!-- 个人中心，反馈 -->
 		<view class="userInfo">
-			
-			<image src="../../static/svg/userCenter.svg" class="icon" @click="toUserInfo"></image>
-			<view style="font-size: 16rpx;"  @click="toUserInfo">个人中心</view>
-			
-			<image src="../../static/svg/callBack.svg" class="icon" @click="toRoutine"></image>
+
+			<image src="../../static/userDeafualt.png" class="icon" @click="toUserInfo"></image>
+			<view style="font-size: 16rpx;" @click="toUserInfo">个人中心</view>
+
+			<image src="../../static/routine.png" class="icon" @click="toRoutine"></image>
 			<view style="font-size: 16rpx;" @click="toRoutine">推荐路线</view>
 		</view>
-		
+
 		<!-- 地图 -->
 		<web-view src="../../static/map.html"></web-view>
-		
+
 		<!-- 底部弹出层 -->
 		<view :class="'tail '+(this.pop?'after_pop':'before_pop')">
-			
+
 			<!-- 搜索栏，拉起按钮 -->
 			<view class="header">
-				<uni-search-bar class="search" @confirm="confirm" @input="input" radius="25"></uni-search-bar>
-				<image :class="'back '+ (this.rotate?'after_r':'before_r')" src="../../static/svg/back_1.svg"
+				<uni-search-bar class="search" @confirm="confirm" @input="input" v-model="text" radius="25"
+					@cancel="cancelSearch"></uni-search-bar>
+				<image :class="'back '+ (this.rotate_pop?'after_r':'before_r')" src="../../static/svg/back_1.svg"
 					@click="popUp">
 				</image>
 			</view>
-			
+
+			<!-- 折叠 -->
+			<view class="fold" @click="foldUp">
+				<image src="../../static/svg/back_1.svg" :class="this.rotate_fold?'after_r':'before_r'"></image>
+			</view>
 			<!-- 存放button的容器 -->
-			<view class="main">
+			<view :class="'main '+(this.fold?'after_fold':'before_fold')">
 				<view class="button">
 					<image src="../../static/logo.png" class="button_img"></image>
 					<view class="font">美食</view>
@@ -55,6 +60,37 @@
 					<image src="../../static/logo.png" class="button_img"></image>
 					<view class="font">美食</view>
 				</view>
+
+			</view>
+			<!-- 存放搜索结果 -->
+			<view class="result" v-if="showResult">
+				<view class="result_item" v-for="val,index in tabledata">
+					<view class="title">{{val.title}}</view>
+					<view class="content">
+						<view class="location">{{val.content}}</view>
+						<view class="comment">
+							<view>
+								<image src="../../static/svg/star.svg" style="width: 20rpx;height: 20rpx;"
+									v-for="v in 5"></image>
+							</view>
+
+							<view>
+								￥13
+							</view>
+							<view>
+								特色菜
+							</view>
+							<view style="background-color: #68c5fe;border-radius: 6rpx;">
+								<view class="font_2">连锁</view>
+							</view>
+						</view>
+						<view class="time">
+							<image src="../../static/svg/time.svg"></image>
+							<view>10:00-22:00</view>
+						</view>
+
+					</view>
+				</view>
 			</view>
 		</view>
 	</view>
@@ -65,9 +101,45 @@
 	export default {
 		data() {
 			return {
-				rotate: false,
+				rotate_pop: false,
+				rotate_fold: false,
 				pop: false,
-				input: ""
+				fold: false,
+				showResult:false,
+				text: "",
+				tabledata: [{
+						title: "烤鸭饭",
+						content: "学一食堂"
+					},
+					{
+						title: "烤鸭饭",
+						content: "学一食堂"
+					}, {
+						title: "烤鸭饭",
+						content: "学一食堂"
+					}, {
+						title: "烤鸭饭",
+						content: "学一食堂"
+					}, {
+						title: "烤鸭饭",
+						content: "学一食堂"
+					}, {
+						title: "烤鸭饭",
+						content: "学一食堂"
+					}, {
+						title: "烤鸭饭",
+						content: "学一食堂"
+					}, {
+						title: "烤鸭饭",
+						content: "学一食堂"
+					}, {
+						title: "烤鸭饭",
+						content: "学一食堂"
+					}, {
+						title: "烤鸭饭",
+						content: "学一食堂"
+					}
+				]
 			}
 		},
 		onLoad() {
@@ -77,36 +149,51 @@
 			confirm() {
 
 			},
-			toUserInfo(){
+			toUserInfo() {
 				uni.navigateTo({
-					url:"/pages/userInfo/userInfo"
+					url: "/pages/userInfo/userInfo",
+
 				})
 			},
-			toRoutine(){
+			toRoutine() {
 				uni.navigateTo({
-					url:"/pages/routine/routine"
+					url: "/pages/routine/routine",
+
 				})
 			},
+			input() {
+					this.pop = true;
+					this.rotate_pop = true;
+					this.fold = true;
+					this.rotate_fold = true;
+					this.showResult=true;
+			},
+			cancelSearch() {},
 			popUp() {
 				this.pop = !this.pop;
-				this.rotate = !this.rotate;
+				this.rotate_pop = !this.rotate_pop;
+			},
+			foldUp() {
+				this.fold = !this.fold;
+				this.rotate_fold = !this.rotate_fold;
 			}
 		}
 	}
 </script>
 
 <style>
-	.content {
+	.page {
 		height: 100%;
 	}
+
 	.userInfo {
 		z-index: 10;
-		width: 65rpx;
+		width: 80rpx;
 		height: 200rpx;
 		padding: 10rpx;
 		background-color: #fff;
 		border-radius: 20rpx;
-		box-shadow:0px 0px 10rpx #c9c9c9;
+		box-shadow: 0px 0px 10rpx #c9c9c9;
 		opacity: 0.9;
 		position: fixed;
 		right: 20rpx;
@@ -116,21 +203,22 @@
 		justify-content: space-around;
 		align-items: center;
 	}
+
 	.tail {
 		z-index: 1;
 		width: 100%;
 		height: 60%;
 		border-radius: 50rpx 50rpx 0 0;
-		box-shadow:0px 0px 25rpx #c9c9c9;
+		box-shadow: 0px 0px 25rpx #c9c9c9;
 		position: fixed;
-		bottom: -43%;
+		bottom: -40%;
 		background-color: #fff;
 		display: flex;
 		flex-direction: column;
 		align-items: center;
 	}
 
-	.icon{
+	.icon {
 		width: 50rpx;
 		height: 50rpx;
 	}
@@ -142,13 +230,30 @@
 		align-items: center;
 		justify-content: space-around;
 	}
-	.main{
+
+	.fold {
 		width: 90%;
-		height: 30%;
-		display: grid;
-		grid-template-columns: repeat(4,1fr);
+		height: 50rpx;
+		display: flex;
+		justify-content: center;
+		border-top: 2rpx solid #e2e3e4;
 	}
-	.button{
+
+	.fold image {
+		width: 40rpx;
+		height: 40rpx;
+	}
+
+	.main {
+		width: 700rpx;
+		height: 400rpx;
+		display: grid;
+		grid-template-columns: repeat(4, 1fr);
+		overflow: hidden;
+		border-bottom: 2rpx solid #e2e3e4;
+	}
+
+	.button {
 		width: 100rpx;
 		height: 140rpx;
 		margin: 30rpx;
@@ -158,14 +263,72 @@
 		justify-content: space-around;
 		align-items: center;
 	}
-	.button_img{
+
+	.button_img {
 		width: 100rpx;
 		height: 100rpx;
 		border-radius: 15rpx;
 	}
-	.font{
+
+	.font {
 		font-size: 24rpx;
 	}
+
+	.result {
+		width: 90%;
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		overflow: scroll;
+	}
+
+	.result_item {
+		width: 100%;
+		min-height: 170rpx;
+		box-sizing: border-box;
+		border-bottom: 2rpx solid #e2e3e4;
+		margin-top: 6rpx;
+	}
+
+	.result_item:active {
+		background-color: #e2e3e4;
+	}
+
+	.title {
+		height: 30%;
+		margin-left: 20rpx;
+		font-size: 36rpx;
+	}
+
+	.content {
+		height: 70%;
+		margin-left: 20rpx;
+		margin-top: 10rpx;
+		font-size: 26rpx;
+	}
+
+	.comment {
+		width: 50%;
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+	}
+	.font_2{
+		color: #015ec8;
+		margin: 3rpx;
+		font-size: 20rpx;
+	}
+	.time{
+		height: 30rpx;
+		display: flex;
+		align-items: center;
+	}
+	.time image{
+		width: 30rpx;
+		height: 30rpx;
+	}
+
 	.search {
 		width: 70%;
 	}
@@ -188,6 +351,15 @@
 		transition: .3s;
 	}
 
+	.before_fold {
+		transition: 0.5s;
+	}
+
+	.after_fold {
+		height: 0;
+		transition: 0.5s;
+	}
+
 	.before_r {
 		transition: .2s;
 	}
@@ -196,6 +368,4 @@
 		transform: rotate(-180deg);
 		transition: .2s;
 	}
-
-
 </style>
--- pages/userInfo/userInfo.vue
+++ pages/userInfo/userInfo.vue
@@ -1,28 +1,31 @@
 <template>
 	<view class="content">
-		<view class="userImg"></view>
+		<view class="userImg">
+			<image src="../../static/userDeafualt.png"></image>
+		</view>
+		<view class="back"> <image src="../../static/userImg_back.png"></image> </view>
 		<view class="tail">
-			<view class="item">
+			<view class="item" @click="toFuc(0)">
 				<view class="font">更改头像</view>
 				<image src="../../static/svg/back_1.svg"></image>
 			</view>
-			<view class="item">
+			<view class="item" @click="toFuc(1)">
 				<view class="font">修改密码</view>
 				<image src="../../static/svg/back_1.svg"></image>
 			</view>
-			<view class="item">
+			<view class="item" @click="toFuc(2)">
 				<view class="font">修改昵称</view>
 				<image src="../../static/svg/back_1.svg"></image>
-			</view> 
-			<view class="item">
+			</view>
+			<view class="item" @click="toFuc(3)">
 				<view class="font">个性签名</view>
 				<image src="../../static/svg/back_1.svg"></image>
 			</view>
-			<view class="item">
+			<view class="item" @click="toFuc(4)">
 				<view class="font">设置</view>
 				<image src="../../static/svg/back_1.svg"></image>
 			</view>
-			<view class="item">
+			<view class="item" @click="toFuc(5)">
 				<view class="font">关于我们</view>
 				<image src="../../static/svg/back_1.svg"></image>
 			</view>
@@ -34,42 +37,64 @@
 	export default {
 		data() {
 			return {
-				Height:0
+				Height: 0
 			}
 		},
 		methods: {
-			
+			toFuc(v){
+				console.log(v);
+			}
 		},
 		mounted() {
 			uni.getSystemInfo({
-			    success: function (res) {
-					this.Height=res.windowHeight
-			        console.log(res.windowWidth);
-			        console.log(res.windowHeight);
-			    }
+				success: function(res) {
+					this.Height = res.windowHeight
+					console.log(res.windowWidth);
+					console.log(res.windowHeight);
+				}
 			});
 		}
 	}
 </script>
 
 <style>
-	.content{
-		width: 100%;
-		height:100vh;
-		box-sizing: border-box;
+	.content {
+		height: 100%;
+		overflow: hidden;
 		background-color: #f1f2f3;
 	}
-	.userImg{
-		height: 30%;
-		background-color: aquamarine;
+
+	.userImg {
+		height: 20%;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		background-color:#F4FDFF ;
 	}
-	.tail{
+
+	.userImg image {
+		width: 200rpx;
+		height: 200rpx;
+		margin: 0 auto;
+	}
+
+	.back {
+		width: 100%;
+		height: 150rpx;
+		
+	}
+	.back image{
+		width: 100%;
+		height: 100%;
+	}
+	.tail {
 		width: 100%;
 		height: 70%;
 		margin: 0 auto;
 		margin-top: 80rpx;
 	}
-	.item{
+
+	.item {
 		width: 100%;
 		height: 100rpx;
 		border-radius: 6rpx;
@@ -79,12 +104,16 @@
 		align-items: center;
 		position: relative;
 	}
-	.font{
+	.item:active{
+		background-color: #e2e3e4;
+	}
+
+	.font {
 		margin-left: 100rpx;
 		font-size: 32rpx;
 	}
-	
-	.item image{
+
+	.item image {
 		width: 30rpx;
 		height: 30rpx;
 		transform: rotate(90deg);
